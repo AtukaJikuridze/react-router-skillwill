a2359642716df00ae0a654a870c4d729856435bf
ModernDesigners <avtojikuridze@gmail.com>
2024-11-29 17:07:46 +0400

    Mini Portf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import myImg from "./d.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="portfolio-box">
+        <div className="image-main">
+          <img src={myImg} />
+        </div>
+        <h1 className="name">Avto Jikuridze</h1>
+        <hr />
+        <p className="description">
+          Hello, i’m Avto! I’m a self-taught front-end developer based in
+          Tbilisi, Georgia.
+          <br /> I can develop responsive websites Transforming my creativity
+          and knowledge into a websites has been s my passion for over a year.{" "}
+          <br /> I always strive to learn about the newest technologies and
+          frameworks.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <hr />
+        <div className="contacts-main">
+          <h2>Contacts :</h2>
+          <div className="contacts">
+            <a href="https://www.linkedin.com/in/avto-jikuridze-a4167b26b/">
+              Linkedin
+            </a>
+            <a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&to=avtojikuridze@gmail.com&body=my-text">
+              Gmail
+            </a>
+            <a href="https://www.facebook.com/jikuridze.atuka">Facebook</a>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
